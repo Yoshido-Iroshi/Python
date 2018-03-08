16d6ccab750f06b45ae306ee9eb082a1937348ac
Yoshido Iroshi <37142149+Yoshido-Iroshi@users.noreply.github.com>
2018-03-09 02:39:07 +0700

    Add files via upload

---
+++ net-7.py
@@ -0,0 +1,226 @@
+import os
+import time
+import sys
+import itertools
+import threading
+
+os.system('cls')
+os.system('color a')
+time.sleep(3)
+def net_7():
+	print()
+	print("               ====================================================")
+	print("               ===================================================")
+	print("               = 9999    99  999999 99999999         999999999  =")
+	print("               = 99 99   99  99        99                  99  =")
+	print("               = 99  99  99  9999      99     9999        99  =")
+	print("               = 99   99 99  99        99     9999       99  =")
+	print("               = 99    9999  999999    99               99  =")
+	print("               =============================================")
+	print("               ============================================")
+	print()
+net_7()
+time.sleep(2)
+id = input("Enter Your ID : ")
+import os
+os.system('cls')
+net_7()
+time.sleep(1.5)
+if id == "Syarif":
+	os.system("color a")
+	print("              ||||.....::::::: Welcome", id, "::::::.....||||")
+	print()
+else:
+	os.system('color a')
+	print("Illegal User. GET OUT")
+	sys.exit()
+time.sleep(2)
+import getpass
+password = getpass.getpass("Please Enter Your Access Code : ")
+if password == "Yo-03":
+	os.system('cls')
+	net_7()
+	time.sleep(3)
+	print("The Algorithm System will : \n\n")
+	print("[*] Checking The User ID\n[*] Checking The Access Code\n[*] Placing The 256-bit AES Encryption\n\n\nIt will take few seconds. So be patient... :,)")
+
+	#Checking The User ID
+	time.sleep(10)
+	os.system('cls')
+	done = False
+	def algorithm():
+	    for c in itertools.cycle([""]):
+	        if done:
+	            break
+	        sys.stdout.write('\rChecking The User ID........          Ok\n' + c)
+	        sys.stdout.flush()
+	        time.sleep(0.1)
+
+	t = threading.Thread(target=algorithm)
+	t.start()
+
+	#long process here
+	time.sleep(7)
+	done = True
+
+	#Checking Access Code
+	time.sleep(0.3)
+	os.system('cls')
+	done = False
+	def algorithm():
+	    for c in itertools.cycle([""]):
+	        if done:
+	            break
+	        sys.stdout.write('\rChecking Access Code........          Ok\n' + c)
+	        sys.stdout.flush()
+	        time.sleep(0.1)
+
+	t = threading.Thread(target=algorithm)
+	t.start()
+
+	#long process here
+	time.sleep(7)
+	done = True
+
+	#Placing The 256-bit AES Encryption 
+	time.sleep(0.3)
+	os.system('cls')
+	done = False
+	def algorithm():
+	    for c in itertools.cycle([""]):
+	        if done:
+	            break
+	        sys.stdout.write('\rPlacing The 256-bit AES Encryption........          Ok\n' + c)
+	        sys.stdout.flush()
+	        time.sleep(0.1)
+
+	t = threading.Thread(target=algorithm)
+	t.start()
+
+	#long process here
+	time.sleep(7)
+	done = True
+
+	#Loading
+	time.sleep(1)
+	os.system('cls')
+	toolbar_width = 65
+
+	# setup toolbar
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	sys.stdout.write("                [%s]" % (" " * toolbar_width))
+	sys.stdout.flush()
+	sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+	for i in range(toolbar_width):
+	    time.sleep(0.08) # do real work here
+	    # update the bar
+	    sys.stdout.write("*")
+	    sys.stdout.flush()
+
+	sys.stdout.write("\n")
+	
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+	time.sleep(2)
+	os.system("cls")
+	time.sleep(1)
+	print("")
+	os.system("cls")
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print("                                    {*} ACCESS GRANTED {*}")
+
+	time.sleep(2)
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+elif password != "Yo-03":
+	print("WRONG PASSWORD")
